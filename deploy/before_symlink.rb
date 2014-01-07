6ec9278da203356e5a90f6694704b2253ab41d6e
Doug Bryant <doug@netinlet.com>
2014-01-06 18:25:02 -0800

    Script was looking for Chef::Provider::File class - made namespace to ruby File

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,4 +1,4 @@
-if File.exists?("#{release_path}/../../shared/config/mongoid.yml")
+if ::File.exists?("#{release_path}/../../shared/config/mongoid.yml")
   run "cp #{release_path}/../../shared/config/mongoid.yml #{release_path}/config/mongoid.yml"
   run "cd #{release_path} && RAILS_ENV=production bundle exec rake db:seed"
 end

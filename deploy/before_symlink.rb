5ea2c719a00506e84a2a0d26a056d829b35c9d7a
Doug Bryant <doug@netinlet.com>
2014-01-06 17:32:16 -0800

    mongoid.yml going once...

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,2 +1,2 @@
-run "cp #{release_path}/../..//config/mongoid.yml #{release_path}/config/mongoid.yml"
+run "cp #{release_path}/../../shared/config/mongoid.yml #{release_path}/config/mongoid.yml"
 run "cd #{release_path} && RAILS_ENV=production bundle exec rake db:seed"

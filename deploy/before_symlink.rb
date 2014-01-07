7d067edb06cd57a0c7b906015305bdd872a63230
Doug Bryant <doug@netinlet.com>
2014-01-06 18:20:15 -0800

    check for files existance before trying to do anything

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,2 +1,4 @@
-run "cp #{release_path}/../../shared/config/mongoid.yml #{release_path}/config/mongoid.yml"
-run "cd #{release_path} && RAILS_ENV=production bundle exec rake db:seed"
+if File.exists?("#{release_path}/../../shared/config/mongoid.yml")
+  run "cp #{release_path}/../../shared/config/mongoid.yml #{release_path}/config/mongoid.yml"
+  run "cd #{release_path} && RAILS_ENV=production bundle exec rake db:seed"
+end
